9ad132158d9d222a974169eea97caa34d4ffc798
Diego Camino Reinoso <diego.camino.r@gmail.com>
2024-03-11 01:57:54 -0500

    Update purchase list items service

--- src/services/purchaseListItem.service.js
+++ src/services/purchaseListItem.service.js
@@ -1,6 +1,7 @@
-// const httpStatus = require('http-status');
+const httpStatus = require('http-status');
 const { PurchaseListItem } = require('../models');
-// const ApiError = require('../utils/ApiError');
+const ApiError = require('../utils/ApiError');
+
 /**
  * Query for purchase list items
  * @param {Object} filter - Mongo filter
@@ -26,7 +27,47 @@ const getPurchaseListItemById = async (purchaseListItemId) => {
   });
 };
 
+/**
+ * Create purchase list item
+ * @param {ObjectId} listId
+ * @returns {Promise<ListItem>}
+ */
+const createPurchaseListItem = async (user, purchaseListId, listItemId) => {
+  const existingPurchaseListItem = await PurchaseListItem.findOne({
+    user: user.id,
+    purchaseList: purchaseListId,
+  });
+
+  if (existingPurchaseListItem) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'List item was already added to the Puchase List');
+  }
+  const purchaseListItemPayload = {
+    user: user.id,
+    purchaseList: purchaseListId,
+    listItem: listItemId,
+    priceAtOrder: [],
+  };
+  const purchaseListItem = new PurchaseListItem(purchaseListItemPayload);
+  await purchaseListItem.save();
+  return purchaseListItem;
+};
+
+/**
+ * Delete purchase list item
+ * @param {ObjectId} listId
+ * @returns {Promise<ListItem>}
+ */
+const deletePurchaseListItemById = async (purchaseListItemId, userId) => {
+  const listItem = await PurchaseListItem.findOne({ _id: purchaseListItemId, user: userId });
+  if (!listItem) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Purchase list item not found');
+  }
+  await listItem.remove();
+};
+
 module.exports = {
   queryPurchaseListItems,
   getPurchaseListItemById,
+  createPurchaseListItem,
+  deletePurchaseListItemById,
 };
